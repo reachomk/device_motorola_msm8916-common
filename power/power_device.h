727ba60b8643f13da91a9eb7df0a03d7c292dc2f
Akianonymus <anonymus.aki@gmail.com>
2019-04-14 07:25:43 -0400

    power_device.h: Let's not underclock

--- power/power_device.h
+++ power/power_device.h
@@ -43,7 +43,7 @@ static power_profile profiles[PROFILE_MAX] = {
         .above_hispeed_delay = 20000,
         .target_loads = 80,
         .target_loads_off = 90,
-        .scaling_max_freq = 1209000,
+        .scaling_max_freq = 1363000,
         .scaling_min_freq = 400000,
         .scaling_min_freq_off = 200000,
     },
