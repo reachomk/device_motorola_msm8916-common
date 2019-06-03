b0f60100608ffb2ae86dde6bbc0688ece8657f50
Om K <reachomk@gmail.com>
2019-06-03 10:02:24 -0700

    Add back libqsap_shim.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -85,3 +85,14 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+# qsap_sdk shim
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libqsap_shim.c
+LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog
+LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
+LOCAL_MODULE := libqsap_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
